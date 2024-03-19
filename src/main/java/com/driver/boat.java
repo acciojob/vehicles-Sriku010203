0f15e45a228defd88c0e847c83e1d624b88cba04
Srikanth Naik <48707298+Sriku010203@users.noreply.github.com>
2024-03-19 10:11:19 +0530

    Update boat.java

--- src/main/java/com/driver/boat.java
+++ src/main/java/com/driver/boat.java
@@ -1,1 +1,15 @@
+public class Boat{
+  String name;
+  int capacity;
+  
+  public String getVehicleName(){
+    return this.name;
+  }
 
+  
+  public int getVehicleCapacity(){
+    return this.capacity;
+  }
+
+  
+}
